5b72ea5249d3704c7923c3b9f48c6b53537a9f8c
Dawid Bogocz <dawibog325@student.polsl.pl>
2020-10-10 11:42:42 +0200

    "Input/Output"

--- test.cpp
+++ test.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hello World" << std::endl;
+
+    int variable; //empty variable to store input
+
+    std::cout << "Input a number" << std::endl;
+
+    std::cin >> variable; //read input from cin and put it in variable
+
+    std::cout << "Your number is: " << variable << std::endl; //print variable using cout
+
     return 0;
 }
