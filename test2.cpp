df82063fd928c0d30dbd53da53932b75ab0e4ba2
Dawid Bogocz <dawibog325@student.polsl.pl>
2020-10-10 12:32:25 +0200

    For loop

---
+++ test2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(){
+
+    int times;
+
+    std::cout << "Please introduce the number of times you want to repeat the sentence" << std::endl;
+    std::cin >> times;
+
+    for(int i = 0 ; i < times ; i++)
+    {
+        std::cout << "I will not talk in class" << std::endl;
+    }
+
+    return 0;
+}
